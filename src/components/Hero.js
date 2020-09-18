94e17bf718f61b6cb3ba4cfc49befeb72ae12ebd
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-18 07:56:59 +0200

    Connecting with Strapi Backend

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,8 +5,42 @@ import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
 
+// Variable:
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+// Main Function:
 const Hero = () => {
-  return <h2>hero component</h2>
+  // Destructure la Query para obtener la FLUID Img
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div className="underline"></div>
+          <h1>I am Tomas</h1>
+          <h4>Self-taught Web Developer</h4>
+          <Link to="/contact" className="btn">
+            Contact Me
+          </Link>
+        </article>
+        <Image fluid={fluid} className="hero-img"></Image>
+      </div>
+    </header>
+  )
 }
 
 export default Hero
