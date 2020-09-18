59b7e3e790000ea811a407c50626a5bbfa2facf0
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-18 08:42:35 +0200

    Jobs DONE

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,76 @@
 import React from "react"
 import Title from "./Title"
-import { FaAngleDoubleRight } from "react-icons/fa"
+import { FaAlignRight, FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+// Query:
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+// Main:
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  // Pasamos la Query
+  const data = useStaticQuery(query)
+  // Desstructure la Query:
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  // console.log(jobs) // Vemos los 3 jobs.
+  // Comenzamos ----->
+  // Primero Hard Code Example:
+  const [value, setValue] = React.useState(0)
+  const { company, position, date, desc } = jobs[value]
+  return (
+    <section className="section jobs">
+      <Title title="Experience"></Title>
+      <div className="jobs-center">
+        {/* Btn Container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.strapiId}
+                className={`job-btn ${index === value && "active-btn"}`}
+                onClick={() => setValue(index)}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* Job Info */}
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc.map(item => {
+            return (
+              <div key={item.id} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{item.name}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        More Info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
