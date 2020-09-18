9cb4bdec30bfed2d33f65fd64c704b5ae353ba74
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-18 10:46:29 +0200

    Projects DONE

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,51 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
+// Main:
+export default ({ data }) => {
+  // console.log(data) // Comprobamos que vemos los objects, 2 projects
+  //  Destructure Projects:
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero> </Hero>
       <Services></Services>
       <Jobs></Jobs>
+      <Projects
+        projects={projects}
+        title="Featured Projects"
+        showLink
+      ></Projects>
     </Layout>
   )
 }
+
+// Projects Query to PASS PROPS inside
 // ...GatsbyImageSharpFluid
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        url
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          title
+          id
+        }
+      }
+    }
+  }
+`
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,47 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+//Main:
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="projects-page">
+        <Projects projects={projects} title="all projects"></Projects>
+      </section>
+    </Layout>
+  )
 }
 
 export default ProjectsPage
+
+// Page Query for ALL PROJECTS:
+// ...GatsbyImageSharpFluid
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        github
+        id
+        description
+        url
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          title
+          id
+        }
+      }
+    }
+  }
+`
